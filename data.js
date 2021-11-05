b351adf3a7c31598156d76e8500bfdd013363926
StepaKyznetsov <80485370+StepaKyznetsov@users.noreply.github.com>
2021-11-05 22:52:26 +0300

    Delete data.js

--- data.js
+++
@@ -1,7 +0,0 @@
-module.exports = Object.freeze({
-    VK: 'https://vk.com/yeahbat00',
-    URL: 'https://opensheet.vercel.app/<YOUR_KEY>/<SHEET_NAME>',
-    KEY: '12I2gvogcsrlr3GvWqAwnrLB4k-yx798bl73tO57yTtM',
-    SHEET_NAME: 'Sheet1',
-    TEACHERS: ['Виталий Владимирович', 'Карина Владимировна','Степан Алексеевич']
-})
